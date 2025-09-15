7a92740c5f59b3da7b019a7e4803f702f3e688ac
Roarasaurus1 <47874766+Roarasaurus1@users.noreply.github.com>
2025-09-15 18:03:27 +1000

    Update session.php

--- session.php
+++ session.php
@@ -1,15 +1,15 @@
 <?php
-        // Start the session
-        session_start();
+    // Start the session
+    session_start();
 
-        // Checks if user is not logged in.
-        if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] != TRUE){
-			$_SESSION['loggedin'] = FALSE;
+    // Checks if user is not logged in.
+    if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] != TRUE){
+        $_SESSION['loggedin'] = FALSE;
 
-			// Redirect to login page if not already there.
-			if($_SERVER['REQUEST_URI'] != '/login.php') {
-				header("location: login.php");
-				die();
-			}
+        // Redirect to login page if not already there.
+	if($_SERVER['REQUEST_URI'] != '/login.php') {
+            header("location: login.php");
+            die();
         }
+    }
 ?>
